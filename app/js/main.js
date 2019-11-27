2cdd47c267f671ac456a1d0e93fe9feb4f07436e
gaidaianastasiia <haidaianastasiia0102@meta.ua>
2019-11-27 18:30:31 +0200

    wow.js and animate.css are adding

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,39 @@
-$(document).ready(function () {
-    $("#toggle").click(function () {
-        $(this).toggleClass("open");
-        $("#nav").toggleClass("opened");
-    });
+$(document).ready(function() {
+  wow = new WOW({
+    mobile: false
+  })
+  wow.init();
 
-    $('.nav__link').click(function () {
-        if ($('#toggle').hasClass('open')) {
-            $('#toggle').removeClass('open');
-        }
-    });
+  $("#toggle").click(function() {
+    $(this).toggleClass("open");
+    $("#nav").toggleClass("opened");
+  });
 
-    $('.nav__link').click(function () {
-        if ($('#nav').hasClass('opened')) {
-            $('#nav').removeClass('opened');
-        }
-    });
+  $('.nav__link').click(function() {
+    if ($('#toggle').hasClass('open')) {
+      $('#toggle').removeClass('open');
+    }
+  });
 
+  $('.nav__link').click(function() {
+    if ($('#nav').hasClass('opened')) {
+      $('#nav').removeClass('opened');
+    }
+  });
 
-    $('.services__slider').slick({
-        mobileFirst: true,
-        autoplaySpeed: 3000,
-        arrows: false,
-        dots: true,
-        pauseOnFocus: false,
-        pauseOnHover: false,
-        autoplay: true,
-        infinite: true,
-        variableWidth: true,
-        responsive: [
-          {
-            breakpoint: 992,
-            settings: 'unslick'
-          }
-        ]
-    });
+  $('.services__slider').slick({
+    mobileFirst: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    autoplay: true,
+    infinite: true,
+    variableWidth: true,
+    responsive: [{
+      breakpoint: 992,
+      settings: 'unslick'
+    }]
+  });
 });
